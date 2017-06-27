c26526490b425ed1fa41c1cf6df2c387ceb03c17
LBoyle <louis_boyle@hotmail.com>
2017-06-27 12:30:35 +0100

    index and show routes

--- 01-flask-api/app.py
+++ 01-flask-api/app.py
@@ -1,9 +1,19 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+languages = [{'name': 'JavaScript'}, {'name': 'Python'}, {'name': 'Ruby'}]
+
 @app.route('/', methods=['GET'])
 def test():
     return jsonify({'message': 'It works!'})
+
+@app.route('/langs', methods=['GET'])
+def langsIndex():
+    return jsonify({'languages': languages})
+
+@app.route('/langs/<string:name>', methods=['GET'])
+def langsOne(name):
+    return jsonify({'language': [language for language in languages if language['name'] == name][0]})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
