0d8b1bd21c1d9ef095b176b1ca234866818d094f
LBoyle <louis_boyle@hotmail.com>
2017-06-27 14:34:45 +0100

    got index route working, hacked at it until it worked

--- 02-postgres-app/app.py
+++ 02-postgres-app/app.py
@@ -1,12 +1,40 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import
-app = Flask(__name__)
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-languages = [{'name': 'JavaScript'}, {'name': 'Python'}, {'name': 'Ruby'}]
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from config import DevelopmentConfig
+
+import os
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+class Example(db.Model):
+    __tabename__ = 'example'
+    id = db.Column('id', db.Integer, primary_key=True)
+    data = db.Column('data', db.Unicode)
+
+    def __init__(self, id, data):
+        self.id = id
+        self.data = data
 
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'message': 'Homepage'})
 
+@app.route('/examples', methods=['GET'])
+def examplesIndex():
+    return jsonify({'examples': [{'id': example.id, 'data': example.data} for example in Example.query.all()]})
+
 if __name__ == '__main__':
+    # manager.run()
     app.run(debug=True, port=8080)
