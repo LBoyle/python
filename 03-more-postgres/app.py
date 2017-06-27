3b3453b433d27cf11fd3c50cc1f28645ad0f2887
LBoyle <louis_boyle@hotmail.com>
2017-06-27 16:21:52 +0100

    working on a simple relationship

---
+++ 03-more-postgres/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(20))
+    pets = db.relationship('Pet', backref='owner', lazy='dynamic')
+# backref is a virtual column
+# lazy allows querying
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(20))
+    owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+# after writing this I used the idle to create the tables
+# db.create_all()
+# db.session.commit()
